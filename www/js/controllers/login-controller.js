8b189cdde4112f879e0a1689ef2302981ca8414c
Ganesh Avhad <ganesh.avhad@wohlig.com>
2018-03-29 15:41:02 +0530

    facebook login

--- www/js/controllers/login-controller.js
+++ www/js/controllers/login-controller.js
@@ -1,16 +1,26 @@
 // // angular.module('starter.controllers', [])
-myApp.controller('LoginCtrl', function($scope, $ionicModal, $timeout, $log, $window,$http,Predict) {
+myApp.controller('LoginCtrl', function($scope, $ionicModal, $timeout, $log, $window,$http,Predict,$cordovaOauth) {
     $log.log("in login controller ");
 
-    $scope.login=function(userData){
-       Predict.callApiWithData("User/login",userData, function (data) {
-      console.log("value",data.data.value)
-        if(data.data.value == true){
-        $.jStorage.set("user",data.data.data._id);
-        console.log(data.data.data._id)
-       }
+  //   $scope.login=function(userData){
+  //      Predict.callApiWithData("User/login",userData, function (data) {
+  //     console.log("value",data.data.value)
+  //       if(data.data.value == true){
+  //       $.jStorage.set("user",data.data.data._id);
+  //       console.log(data.data.data._id)
+  //      }
   
-      // if($scope.teamSlider.betName==IstInningScore){
-  })
-  }
+  //     // if($scope.teamSlider.betName==IstInningScore){
+  // })
+  // }
+$scope.facebookLogin = function() {
+        $cordovaOauth.facebook("299757727221295", ["email"]).then(function(result) {
+            // results
+            console.log("data",result)
+        }, function(error) {
+            // error
+        });
+    }
+    
+
 })
--- www/js/controllers/profile-controller.js
+++ www/js/controllers/profile-controller.js
@@ -1,3 +1,7 @@
-myApp.controller('ProfileCtrl', function($scope, $log, $window) {
-
+myApp.controller('ProfileCtrl', function($scope, $log, $window,Predict,$http) {
+    var userId = $.jStorage.get('user');
+    Predict.callApiWithData("User/getone",{_id:userId},function(userData){
+console.log("userData",userData.data.data);
+$scope.userDetail=userData.data.data
+    })
 })
