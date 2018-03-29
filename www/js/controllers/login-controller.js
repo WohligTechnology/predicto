15b022348c948b1fdb41881f040cb87c8dbecca4
Ganesh Avhad <ganesh.avhad@wohlig.com>
2018-03-29 16:24:33 +0530

    facebook login

--- www/js/controllers/login-controller.js
+++ www/js/controllers/login-controller.js
@@ -13,14 +13,57 @@ myApp.controller('LoginCtrl', function($scope, $ionicModal, $timeout, $log, $win
   //     // if($scope.teamSlider.betName==IstInningScore){
   // })
   // }
+// $scope.facebookLogin = function() {
+//     console.log("data")
+//         $cordovaOauth.facebook("299757727221295", ["email"]).then(function(result) {
+//             // results
+//             console.log("data",result)
+//         }, function(error) {
+//             // error
+//         });
+//     }
 $scope.facebookLogin = function() {
-        $cordovaOauth.facebook("299757727221295", ["email"]).then(function(result) {
-            // results
-            console.log("data",result)
-        }, function(error) {
-            // error
+ $cordovaOauth.facebook("299757727221295", ["email", "user_location", "user_relationships"]).then(function (result) {
+      console.log("Response Object -> " + JSON.stringify(result));
+      console.log("facebookLogin", result)
+      $.jStorage.set("socialLogin", result);
+      $scope.socialLogin = $.jStorage.get("socialLogin")
+      console.log($scope.socialLogin)
+      if ($scope.socialLogin.access_token != '') {
+        $http.get("https://graph.facebook.com/v2.5/me", {
+          params: {
+            access_token: $scope.socialLogin.access_token,
+            fields: "id,name,email,gender,location,website,picture,relationship_status",
+            format: "json"
+          }
+        }).then(function (result) {
+          $scope.profileData = result.data;
+          // var Socialstate = result.data.location.name.split(",")
+          $scope.socialLoginData = {
+            name: $scope.profileData.name,
+            email: $scope.profileData.email,
+            socailLoginPhoto: $scope.profileData.picture.data.url,
+            // state: Socialstate[1],
+            // city: Socialstate[0],
+            // country: "India"
+          }
+     
+        }, function (error) {
+          alert("There was a problem getting your profile. Check the logs for details.");
+          console.log(error);
         });
-    }
+
+      } else {
+        alert("Not signed in");
+        $state.go("login");
+      }
+      //  $state.go("signUp")
+      // results
+    }, function (error) {
+      console.log("facebook login crashed")
+    })
+
+  }
     
 
 })
