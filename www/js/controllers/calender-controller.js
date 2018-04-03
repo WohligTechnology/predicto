d386710d765137fa22e60446d4bdb088fc89bab5
pravinwohlig <pravin.mane@wohlig.com>
2018-04-03 15:41:00 +0530

    starting match time done

--- www/js/controllers/calender-controller.js
+++ www/js/controllers/calender-controller.js
@@ -1,18 +1,7 @@
-myApp.controller('CalenderCtrl', function($scope, $log, $window,Predict,$http, $stateParams, $filter) {
-    var date = new Date();
-    var  ddMMyyy = $filter('date')(date, 'dd/MM/yyyy HH:mm');
-    $log.log("System date", ddMMyyy);
-
-Predict.callApiWithoutData("Match/allMatches",function(data){
-    $scope.matchList=data.data.data;
-    // console.log("$scope.matchList", $scope.matchList);
-
-    // for (var i = 0; i < $scope.matchList.length; i++) {
-    //   //  $scope.matchList[i].startingTime = $filter("date")( $scope.matchList[i].startingTime);
-    //   var filterTime =  $filter("date")( $scope.matchList[i].startingTime, 'HH:mm');
-    //  // var minusTime = filterTime - 30;
-    //     $log.log("Time in filter", filterTime);
-    // }
-})
+myApp.controller('CalenderCtrl', function ($scope, $log, $window, Predict, $http, $stateParams, $filter) {
+   
+    Predict.callApiWithoutData("Match/allMatches", function (data) {
+        $scope.matchList = data.data.data;
+    })
 
 })
--- www/js/controllers/home-controller.js
+++ www/js/controllers/home-controller.js
@@ -1,9 +1,24 @@
 
 myApp.controller('HomeCtrl', function ($scope, $log, $window, Predict, $stateParams) {
     console.log($stateParams.id)
-
+    $scope.matchRestriction=false
     var userId = $.jStorage.get('user');
     var matchId = $stateParams.id
+    Predict.callApiWithData("Match/getone", { _id: matchId }, function (data) {
+        $scope.matchName = data.data.data
+        console.log("***********************************", data)
+        var endTime=moment(new Date()).add(30, 'minutes').format('MMMM Do YYYY, h:mm:ss a');
+        var startTime=moment($scope.matchName.startingTime).format('MMMM Do YYYY, h:mm:ss a');
+        console.log("match time before half hour", startTime)
+        
+        
+        console.log("system time",endTime)
+        if(startTime<=endTime){
+
+            $scope.matchRestriction=true
+        }
+    })
+
     // $scope.IstInningScore = false;
     // $scope.tossWinner = false;
     // $scope.Winner = false;
@@ -27,16 +42,13 @@ myApp.controller('HomeCtrl', function ($scope, $log, $window, Predict, $statePar
         console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%", data)
         // if($scope.teamSlider.betName==IstInningScore){
     })
-    Predict.callApiWithData("Match/getone", { _id: matchId }, function (data) {
-        $scope.matchName = data.data.data
-        console.log("***********************************", data)
-    })
 
     $scope.predict = function (bets, data) {
         console.log("bets", data)
         bets.betType = data._id
         bets.match = matchId
         bets.user = userId
+        bets.betDone=true
         Predict.callApiWithData("UserBets/save", bets, function (data) {
             console.log("$$$$$$$$$$$$$$$", data)
             // if($scope.teamSlider.betName==IstInningScore){
