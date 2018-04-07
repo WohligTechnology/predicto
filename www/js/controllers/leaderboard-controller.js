81a8a74df8c2f067d803d9cdb688fb09581a2c4f
pravinwohlig <pravin.mane@wohlig.com>
2018-04-07 16:58:19 +0530

    state reload menu

--- www/js/controllers/leaderboard-controller.js
+++ www/js/controllers/leaderboard-controller.js
@@ -2,6 +2,7 @@ myApp.controller('LeaderBoardCtrl', function($scope, $log, $window,Predict,$http
 Predict.callApiWithoutData("User/leaderboard",function(data){
     console.log(data.data.data)
     $scope.leaderboard=data.data.data
+    $scope.photo="https://graph.facebook.com/"+data.data.data.userId+"/picture?width=1024&height=1024";
     // if($scope.leaderboard.points==null){
     //     $scope.leaderboard.points=0
     // }
